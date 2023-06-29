aa246de393b8621df433c21914cdb0061350b02e
Augustin ROUSSET-ROUVIERE <augustin.rousset-rouviere@isep.fr>
2023-06-29 17:45:17 +0200

    [#6][RFC#4] - Add a function to test if a token is a resolution or not
    [+] Function to test if a token is a resolution or not
    [+] Add test for this parsing function

--- src/parsing.rs
+++ src/parsing.rs
@@ -1,10 +1,16 @@
-use crate:: utils::is_hexa;
+use regex::Regex;
+
+use crate::utils::is_hexa;
 
 use crate::elements::Category;
 use crate::errors::ParsingError;
 use crate::utils::is_digit;
 use crate::{elements::Elements, utils::is_hexa};
+pub fn is_crc32(sub_tolen: &str) -> bool {
+    is_hexa(sub_tolen) && sub_tolen.len() == 8
+}
 
-pub fn is_crc32(crc32: &str) -> bool {
-    is_hexa(crc32) && crc32.len() == 8
+pub fn is_resolution(sub_tolen: &str) -> bool {
+    let resolution_regex = Regex::new(r"\d{3,4}([pP]|([xX\u00D7]\d{3,4}))$|^[248]K$").unwrap();
+    resolution_regex.is_match(sub_tolen)
 }
--- tests/parsing.rs
+++ tests/parsing.rs
@@ -1,4 +1,4 @@
-use anitomy_rust::parsing::is_crc32;
+use anitomy_rust::parsing::{is_crc32, is_resolution};
 
 #[test]
 fn crc32() {
@@ -7,4 +7,14 @@ fn crc32() {
     assert!(!is_crc32("8F5GF2BA"));
     assert!(!is_crc32("8F50F2BAA"));
     assert!(!is_crc32("8F50F2B"));
+}
+
+#[test]
+fn resolution(){
+    assert!(is_resolution("1080p"));
+    assert!(is_resolution("2K"));
+    assert!(is_resolution("1920x1080"));
+    assert!(is_resolution("720p"));
+    assert!(!is_resolution("Hello world"));
+    assert!(is_resolution("1080×720"));
 }
