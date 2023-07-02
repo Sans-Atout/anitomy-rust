1a1e5da0b87570d921deed66172912fa920d2783
Augustin ROUSSET-ROUVIERE <augustin.rousset-rouviere@isep.fr>
2023-07-02 20:45:34 +0200

    [TEST] - Create test for all category parsing
    [*] Rename parsing to parsing_function
    
    [+] Add test for Anime Season category
    [+] Add test for Anime Season Prefix category
    [+] Add test for Anime Title category
    [+] Add test for Anime Type category
    [+] Add test for Anime Year category
    [+] Add test for Audio Term category
    [+] Add test for Device Compatibility category
    [+] Add test for Episode Number category
    [+] Add test for Episode Number Alt category
    [+] Add test for Episode Prefix category
    [+] Add test for Episode Title category
    [+] Add test for File Checksum category
    [+] Add test for File Extension category
    [+] Add test for File Name category
    [+] Add test for Language category
    [+] Add test for Other category
    [+] Add test for Release Group category
    [+] Add test for Release Information category
    [+] Add test for Release Version category
    [+] Add test for Source category
    [+] Add test for Subtitles category
    [+] Add test for Video Resolution category
    [+] Add test for Video Term category
    [+] Add test for Volume Number category
    [+] Add test for Volume Prefix category
    [+] Add test for Unknown category

---
+++ tests/parsing_test.rs
@@ -0,0 +1,109 @@
+use anitomy_rust::{
+    elements::{Category, Elements, Element},
+    Parser,
+};
+
+#[test]
+fn testing_anime_season() {
+    // TODO
+}
+#[test]
+fn testing_anime_season_prefix() {
+    // TODO
+}
+#[test]
+fn testing_anime_title() {
+    // TODO
+}
+#[test]
+fn testing_anime_type() {
+    // TODO
+}
+#[test]
+fn testing_anime_year() {
+    // TODO
+}
+#[test]
+fn testing_audio_term() {
+    // TODO
+}
+#[test]
+fn testing_device_compatibility() {
+    // TODO
+}
+#[test]
+fn testing_episode_number() {
+    // TODO
+}
+#[test]
+fn testing_episode_number_alt() {
+    // TODO
+}
+#[test]
+fn testing_episode_prefix() {
+    // TODO
+}
+#[test]
+fn testing_episode_title() {
+    // TODO
+}
+#[test]
+fn testing_file_checksum() {
+    // TODO
+}
+#[test]
+fn testing_file_extension() {
+    // TODO
+}
+#[test]
+fn testing_file_name() {
+    // TODO
+}
+#[test]
+fn testing_language() {
+    // TODO
+}
+#[test]
+fn testing_other() {
+    // TODO
+}
+#[test]
+fn testing_release_group() {
+    // TODO
+}
+#[test]
+fn testing_release_information() {
+    // TODO
+}
+#[test]
+fn testing_release_version() {
+    // TODO
+}
+#[test]
+fn testing_source() {
+    // TODO
+}
+#[test]
+fn testing_subtitles() {
+    // TODO
+}
+#[test]
+fn testing_video_resolution() {
+    // TODO
+}
+#[test]
+fn testing_video_term() {
+    // TODO
+}
+#[test]
+fn testing_volume_number() {
+    // TODO
+}
+#[test]
+fn testing_volume_prefix() {
+    // TODO
+}
+#[test]
+fn testing_unknown() {
+    // TODO
+}
